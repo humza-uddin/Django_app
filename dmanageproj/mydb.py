84f0d227641bdf91d6ae4f09e54ea93339d5ab01
humza-uddin <thereal.humza@gmail.com>
2023-08-12 18:11:00 +0500

    Initial Commit

---
+++ dmanageproj/mydb.py
@@ -0,0 +1,20 @@
+#install mysql
+#pip install mysql
+#pip install mysql-connector
+#---Pip install mysql-connector-python
+
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'Powerful@123'
+)   
+
+#prepare a cursor object
+cursorObject = dataBase.cursor()
+
+# Create a database
+cursorObject.execute("CREATE DATABASE VisumAcura")
+
+print("Database has been Created!")
